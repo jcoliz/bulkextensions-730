45643a151ed7c4af282e2f527ad0630efd1db991
James Coliz <jcoliz@outlook.com>
2022-03-23 10:40:04 -0700

    refactor: Remove needless ApplicationDbContext constructor

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,10 +7,6 @@ public class ApplicationDbContext : DbContext
     public DbSet<Parent> Parents { get; set; }
     public DbSet<Child> Children { get; set; }
 
-    public ApplicationDbContext()
-    {
-    }
-
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
